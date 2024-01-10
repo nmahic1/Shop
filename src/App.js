bb89fc61f70657352b92fd35ebf31a7bb8ce8615
Nejla Mahić <nmahic1@gmail.com>
2024-01-10 17:15:55 +0100

    adding components and screens

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import AboutUs from "./Screens/AboutUs/AboutUs";
+import Contacts from "./Screens/Contacts/Contacts";
+import Landing from "./Screens/Landing/Landing";
+import Products from "./Screens/Products/Products";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="/aboutUs" element={<AboutUs />}></Route>
+        <Route path="/contact" element={<Contacts />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+      </Routes>
+      <Footer></Footer>
+    </BrowserRouter>
+  );
 }
 
 export default App;
